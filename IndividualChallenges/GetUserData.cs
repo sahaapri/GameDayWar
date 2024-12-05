eac3f2d90c94531439bf608a591aec6190705931
sandeepmed2 <55948296+sandeepmed2@users.noreply.github.com>
2024-12-05 16:09:39 +0530

    Update GetUserData.cs

--- IndividualChallenges/GetUserData.cs
+++ IndividualChallenges/GetUserData.cs
@@ -1,19 +1,22 @@
 public void GetUserData(int id)
 {
     // SQL injection vulnerability
-    string connectionString = "YourConnectionStringHere";
-    string query = $"SELECT * FROM Users WHERE id={id}";
+    string connectionString = "YourConnectionStringHere";    
+    string query = "SELECT * FROM Users WHERE id=@id";
 
     using (var connection = new SqlConnection(connectionString))
     {
         connection.Open();
         using (var command = new SqlCommand(query, connection))
         {
-            using (var reader = command.ExecuteReader())
+            command.Parameters.AddWithValue("@id", id);
             {
-                while (reader.Read())
+                using (var reader = command.ExecuteReader())
                 {
-                    Console.WriteLine($"User ID: {reader["Id"]}, Username: {reader["Username"]}");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"User ID: {reader["Id"]}, Username: {reader["Username"]}");
+                    }
                 }
             }
         }
