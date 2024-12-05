01bb0e938bfde810deb5f49c70d3cd2928cb0704
sahaapri <priyam.saha87@gmail.com>
2024-12-05 16:11:25 +0530

    Update DeleteFile.cs

--- IndividualChallenges/DeleteFile.cs
+++ IndividualChallenges/DeleteFile.cs
@@ -1,6 +1,12 @@
 public void DeleteFile(string userInput)
 {
-    // Deletes a file based on unvalidated user input
-    File.Delete(userInput); 
-    Console.WriteLine($"Deleted file: {userInput}");
+    if (File.Exists(userInput))
+    {
+        File.Delete(userInput);
+        Console.WriteLine($"Deleted file: {userInput}");
+    }
+    else
+    {
+        Console.WriteLine($"File not found: {userInput}");
+    }
 }
