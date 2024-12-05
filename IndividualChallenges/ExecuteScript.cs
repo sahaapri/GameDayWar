8810692eafdb5780ba2746bd33600f775581d4a7
teckrookie <102778294+teckrookie@users.noreply.github.com>
2024-12-05 16:05:54 +0530

    Update ExecuteScript.cs

--- IndividualChallenges/ExecuteScript.cs
+++ IndividualChallenges/ExecuteScript.cs
@@ -1,7 +1,7 @@
 public void ExecuteScript(string userInput)
 {
     // Hypothetical scripting engine
-    string script = $"eval({userInput})";
+    string safeInput = HttpUtility.HtmlEncode(userInput); 
     dynamic engine = Activator.CreateInstance(Type.GetType("HypotheticalScriptingEngine"));
     engine.Execute(script);
 }
